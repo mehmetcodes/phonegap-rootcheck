f586ae95d16b9f23e94d0c5d80aea54899bf2d11
Mehmet Yilmaz <mehmetcodes@gmail.com>
2015-09-28 10:35:57 -0400

    Hopefully some final touches on the iOS side

--- src/ios/RootCheckPlugin.h
+++ src/ios/RootCheckPlugin.h
@@ -4,7 +4,10 @@
 @interface RootCheckPlugin : CDVPlugin{
   
 }
+inline void weHaveA_JBProblem() __attribute__((always_inline));
+inline void weHaveA_DBGProblem() __attribute__((always_inline));
 
 -(void)Rootcheck:(CDVInvokedUrlCommand*)command;
-
++(void) jbCheck;
++(void) dbgCheck;
 @end
--- src/ios/RootCheckPlugin.m
+++ src/ios/RootCheckPlugin.m
@@ -8,6 +8,74 @@
 {
     CDVPluginResult* pluginResult = nil;
     NSString* myarg = [command.arguments objectAtIndex:0];
-    pluginResult = [CDVPluginResult    resultWithStatus:@"result"];
+#ifndef DEBUG    
+    [RootCheckPlugin jbCheck];
+    [RootCheckPlugin dbgCheck];
+#endif
+    pluginResult = [CDVPluginResult    resultWithStatus:@"false"];
 }
+
++(void) jbCheck {
+    
+    //**
+    //** JB test
+        
+    //-----------------------------------
+    // call JB weHaveAProblem
+    //-----------------------------------
+    cbBlock chkCallback  = ^{
+            
+        __weak id weakSelf = self;
+        
+        if (weakSelf) weHaveA_JBProblem();
+    };
+        
+    //-----------------------------------
+    // jailbreak detection
+    //-----------------------------------
+    checkFork(chkCallback);
+    checkFiles(chkCallback);
+    checkLinks(chkCallback);
+
+}
+
++(void) dbgCheck {
+    //-------------------------------------------
+    // do not allow debuggers
+    //-------------------------------------------
+    //dbgStop;
+    
+    //-------------------------------------------
+    // check for the presence of a debugger
+    // call weHaveAProblem if there is one
+    //-------------------------------------------
+    cbBlock dbChkCallback  = ^{
+        
+        __weak id weakSelf = self;
+        
+        if (weakSelf) weHaveA_DBGProblem();
+        
+    };
+    
+    dbgCheck(dbChkCallback);
+
+}
+
 @end
+
+//--------------------------------------------------------------------
+// if the device is jailbroken then this method will be called
+//--------------------------------------------------------------------
+void weHaveA_JBProblem() {
+    NSLog(@"RC: True");
+    exit(0);
+}
+
+
+//--------------------------------------------------------------------
+// if a debugger is attched to the app then this method will be called
+//--------------------------------------------------------------------
+void weHaveA_DBGProblem() {
+    NSLog(@"RC: debugger detected");
+    exit(0);
+}
